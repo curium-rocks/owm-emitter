5dbf8dd081a173e512b09159314e3cb7404a8156
Andrew Boyd <akboyd88@users.noreply.github.com>
2021-07-18 11:01:00 -0500

    add readme

---
+++ src/lib.ts
@@ -0,0 +1,3 @@
+export {OneCallApiResponse} from '@curium.rocks/openweathermap-client';
+export {IDataEvent} from '@curium.rocks/data-emitter-base';
+export {OwmEmitter, OwmEmitterOptions} from './owmEmitter';
