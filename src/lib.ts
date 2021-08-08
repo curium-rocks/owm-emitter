3d5f9663a4618037fab5708870364c51db46b275
Andrew Boyd <8363252+bryopsida@users.noreply.github.com>
2021-08-07 19:21:23 -0500

    add emitter factory to support latest base version

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,4 @@
 export {OneCallApiResponse} from '@curium.rocks/openweathermap-client';
 export {IDataEvent} from '@curium.rocks/data-emitter-base';
 export {OwmEmitter, OwmEmitterOptions} from './owmEmitter';
+export {OwmEmitterFactory} from './owmEmitterFactory';
